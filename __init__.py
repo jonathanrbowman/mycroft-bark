23eff3f1600a7ca142310278c3a06a46640dd980
Jonathan Bowman <bowman@swiftbunny.com>
2022-01-22 15:01:10 -0500

    Init

---
+++ __init__.py
@@ -0,0 +1,14 @@
+from mycroft import MycroftSkill, intent_file_handler
+
+
+class TestBark(MycroftSkill):
+    def __init__(self):
+        MycroftSkill.__init__(self)
+
+    @intent_file_handler('bark.intent')
+    def handle_bark(self, message):
+        self.speak_dialog('bark')
+
+
+def create_skill():
+    return TestBark()
